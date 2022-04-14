81e8cb4d993151903cf9da1129cf1636c87a3986
samuel diaz <samuel@gmail.com>
2022-04-14 15:23:40 -0500

    remove unused functions and signal from app.tsx

--- solid-app-2/src/App.tsx
+++ solid-app-2/src/App.tsx
@@ -1,18 +1,10 @@
 import { Routes, Route, Router } from 'solid-app-router'
-import { createSignal } from 'solid-js';
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import ProductDetail from "./views/ProductDetail";
 
 
 function App() {
-  const [search, setSearch] = createSignal('')
-
-  const handleSearch = (value: string) => {
-    setSearch(value)
-  }
-
-
   return (
     <main className="max-w-4xl m-auto">
       <Navbar />
